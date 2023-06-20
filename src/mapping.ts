139668a237622d621a6dd910957371ec5b1c01a5
padidev-tuannt1 <padidev-tuannt1@paditech.com>
2023-06-20 16:19:13 +0700

    modify userInfo

--- src/mapping.ts
+++ src/mapping.ts
@@ -15,32 +15,39 @@ export function handleTokensDebt(event: TokensDebtEvent): void {
   let idoPool = IDOPool.load(event.address.toHex());
   if (idoPool !== null) {
     // Create a new UserInfo entity if it doesn't exist
-    let userInfo = UserInfo.load(event.params.holder.toHex())
+    let userInfo = UserInfo.load(event.params.holder.toHex() + "-" + event.address.toHex())
     if(userInfo == null){
-      userInfo = new UserInfo(event.params.holder.toHex());
+      userInfo = new UserInfo(event.params.holder.toHex() + "-" + event.address.toHex());
       userInfo.debt =  event.params.tokenAmount;
       userInfo.total = event.params.tokenAmount;
       userInfo.totalInvestedETH = event.params.ethAmount;
       userInfo.IDOPool = idoPool.id;
-      idoPool.totalInvestedETH = idoPool.totalInvestedETH.plus(event.params.ethAmount)
-      idoPool.tokensForDistribution = idoPool.tokensForDistribution.plus(event.params.tokenAmount)
-      idoPool.unsold = fetchUnsoldToken(event.address)
-  
       userInfo.save();
-      idoPool.save()
+    } else{
+      userInfo.debt =  userInfo.debt.plus(event.params.tokenAmount);
+      userInfo.total = userInfo.total.plus(event.params.tokenAmount);
+      userInfo.totalInvestedETH = userInfo.totalInvestedETH.plus(event.params.ethAmount);
+      userInfo.save();      
     }
+    idoPool.totalInvestedETH = idoPool.totalInvestedETH.plus(event.params.ethAmount)
+    idoPool.tokensForDistribution = idoPool.tokensForDistribution.plus(event.params.tokenAmount)
+    idoPool.unsold = fetchUnsoldToken(event.address)
+    idoPool.save()
   }
 }
 export function handleTokensWithdrawn(event: TokensWithdrawnEvent): void {
   let idoPool = IDOPool.load(event.address.toHex());
   if (idoPool !== null) {
-    // Create a new UserInfo entity if it doesn't exist
-    let userInfo = new UserInfo(event.params.holder.toHex());
-    userInfo.debt = BigInt.fromI32(0)
+    let userInfo = UserInfo.load(event.params.holder.toHex() + "-" + event.address.toHex())
+    if(userInfo !==null){
+      userInfo.debt = BigInt.fromI32(0)
+      userInfo.save();
+    }
+   
     idoPool.distributedTokens = idoPool.distributedTokens.plus(event.params.amount)
     idoPool.unsold = fetchUnsoldToken(event.address)
 
-    userInfo.save();
+   
     idoPool.save();
   }
 }
@@ -48,10 +55,11 @@ export function handleWithdrawNotSoldToken(event: WithdrawNotSoldTokenEvent): vo
   let idoPool = IDOPool.load(event.address.toHex());
   if(idoPool !== null){
     idoPool.unsold = BigInt.fromI32(0)
+    idoPool.save()
   }
 }
 export function handleRefund(event: RefundUserEvent): void{
-  let userInfo = UserInfo.load(event.params.user.toHex())
+  let userInfo = UserInfo.load(event.params.user.toHex() + "-" + event.address.toHex())
   if(userInfo !== null){
     userInfo.debt = BigInt.fromI32(0)
     userInfo.totalInvestedETH = BigInt.fromI32(0)
